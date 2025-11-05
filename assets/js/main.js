8a66c773b079f15c904c6105fcb55aa3edca14f3
Maksym Radchenko <maksikchipic@gmail.com>
2025-11-05 15:19:30 +0100

    made extra navbar and schedule section

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+// we grab our header and our desktop header
+// we insert the contents of our header into the desktop one
+
+const header = document.querySelector('.header')
+const desktopHeader = document.querySelector('.header-desktop')
+desktopHeader.innerHTML = header.innerHTML
+
+// 1. when the .header enters the viewprt, hide the desktop header (by adding the visible class)
+// 2. when the header leaves, show it (by adding the visible class)
+
+inView('.header')
+    .on('enter', el => desktopHeader.classList.remove('visible'))
+    .on('exit', el => desktopHeader.classList.add('visible'))
