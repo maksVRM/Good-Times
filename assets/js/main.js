8c815fe2770cf5b20524ab46d3067b58de0c08b2
Maksym Radchenko <maksikchipic@gmail.com>
2025-11-05 16:44:04 +0100

    made paralax tilt hover effect for images and scroll opacity effect

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,18 @@ desktopHeader.innerHTML = header.innerHTML
 inView('.header')
     .on('enter', el => desktopHeader.classList.remove('visible'))
     .on('exit', el => desktopHeader.classList.add('visible'))
+
+
+// here we enable the tilt library on all our images
+VanillaTilt.init(document.querySelectorAll('.image'), {
+    max: 25,
+    speed: 400
+})
+
+
+// here we grab all our images we want to fade in
+// we add the visible class which toggles the opacity
+inView('.fade')
+    .on('enter', img => img.classList.add('visible'))
+    .on('exit', img => img.classList.remove('visible'))
+
